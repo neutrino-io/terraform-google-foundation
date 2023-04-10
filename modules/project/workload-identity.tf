1f4e6a436507acb46156ea5e8b489e5a0579c395
Azri Jamil <azri@nematix.com>
2023-04-10 22:17:07 +0800

    fixed: add required param for wid-terraform

--- modules/project/workload-identity.tf
+++ modules/project/workload-identity.tf
@@ -10,10 +10,20 @@ resource "google_iam_workload_identity_pool_provider" "terraform-cloud" {
   project                            = module.app_project.project_id
   workload_identity_pool_id          = google_iam_workload_identity_pool.default.workload_identity_pool_id
   workload_identity_pool_provider_id = "terraform-cloud"
+  display_name                       = "OIDC identity pool provider for Terraform Cloud"
+  attribute_condition                = "assertion.terraform_organization_id == \"${var.terraform_organization_id}\""
   attribute_mapping                  = {
-    "google.subject"                      = "assertion.terraform_workspace_id"
+    "google.subject"                        = "assertion.sub"
+    "attribute.terraform_run_phase"         = "assertion.terraform_run_phase"
+    "attribute.terraform_project_id"        = "assertion.terraform_project_id",
+    "attribute.terraform_project_name"      = "assertion.terraform_project_name",
+    "attribute.terraform_workspace_id"      = "assertion.terraform_workspace_id"
+    "attribute.terraform_workspace_name"    = "assertion.terraform_workspace_name"
+    "attribute.terraform_organization_id"   = "assertion.terraform_organization_id"
+    "attribute.terraform_organization_name" = "assertion.terraform_organization_name"
+    "attribute.terraform_run_id"            = "assertion.terraform_run_id"
+    "attribute.terraform_full_workspace"    = "assertion.terraform_full_workspace"
   }
-  attribute_condition = "assertion.terraform_organization_id == \"${var.terraform_organization_id}\""
 
   oidc {
     issuer_uri = "https://app.terraform.io"
